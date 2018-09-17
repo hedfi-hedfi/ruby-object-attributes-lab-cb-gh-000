7bfbd576215478e96b81a1f42ebe15d2419ed6ad
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-17 18:23:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,21 @@
 class Dog
-  def initialize(name)
-    @this_dog_name = name
+  def initialize(name, breed)
+    @name = name
+    @breed = bredd  
   end
 
   def name=(name)
-    @this_dog_name = name
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
   end
 end
